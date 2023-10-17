b9c29dfe3967ec2d41fa4b2fd1d460a25cddb85c
Bahadır Koramaz <bkork1maz@gmail.com>
2023-10-17 19:31:11 +0300

    17.10.23

--- src/kisiselKullanım/GithubKullanımı1.java
+++ src/kisiselKullanım/GithubKullanımı1.java
@@ -6,7 +6,7 @@ public class GithubKullanımı1 {
         System.out.println("hello world");
 
         System.out.println("thats it");
-
+        System.out.println("main branch de değişiklik 1. satır ");
         System.out.println("bahadır branch de ortak classda değişiklik yaptım");
     }
 }
