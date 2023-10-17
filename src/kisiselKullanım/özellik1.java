284b1416b2aa6e1b57885521263ad30f3315117b
Bahadır Koramaz <bkork1maz@gmail.com>
2023-10-17 19:35:15 +0300

    calışma

--- src/kisiselKullanım/özellik1.java
+++ src/kisiselKullanım/özellik1.java
@@ -1,10 +1,16 @@
 package kisiselKullanım;
 
 public class özellik1 {
+
     public static void main(String[] args) {
 
         System.out.println("bahadır branch da çalışıyorum");
 
         System.out.println("bahadır branch de ikinci değişiklik öxel class da");
+
+        System.out.println("bahadır branch çalışma 2");
+
+        System.out.println("git-github çalışıyorum");
+
     }
 }
